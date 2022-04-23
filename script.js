f32e27943701d17504002cc5d6d7834f6a5e1c4d
AJ Jaswal <jaswalakshay@yahoo.com>
2022-04-23 17:35:14 -0400

    added Q's from w3schools and questionarray

--- script.js
+++ script.js
@@ -19,5 +19,36 @@ var initialsEl = document.getElementById("initials");
 var highscoresEL = document.getElementById("highscores");
 var highscoreArray = [];
 
+// questions
+var question0 = {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer1: "<javascript>",
+    answer2: "<js>",
+    answer3: "<scripting>",
+    rightAnswer: "<script>"
+  };
+  var question1 = {
+    question: "Where is the correct place to insert a JavaScript?",
+    answer1: "The stylesheet",
+    answer2: "The <body> section",
+    answer3: "The <head> section",
+    rightAnswer: "The <head> section or the <body> section"
+  };
+  var question2 = {
+    question: "How do you write 'Hello World' in an alert box?",
+    answer1: "msg('Hello World');",
+    answer2: "msgBox('Hello World');",
+    answer3: "alertBox('Hello World');",
+    rightAnswer: "alert('Hello World');"
+  };
+  var question3 = {
+    question: "What special character are javascript arrays contained in?",
+    answer1: "' '",
+    answer2: "( )",
+    answer3: "{ }",
+    rightAnswer: "[ ]"
+  };
 
+  var questionArray = [question0, question1, question2, question3];
+  var questionIndex = 0;
 
