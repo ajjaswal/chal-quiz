ead12fc81eed67fdb0445adbe154ed5fafb2ce85
AJ Jaswal <jaswalakshay@yahoo.com>
2022-04-23 17:34:01 -0400

    finished page elements

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ var backButton = document.getElementById("back-button");
 var submitButton = document.getElementById("submit-button");
 var clearButton = document.getElementById("clear-button");
 
+var homePage = document.getElementById("home-page");
+var questionPage = document.getElementById("question-page");
+var scorePage = document.getElementById("score-page");
+var highscorePage = document.getElementById("highscore-page");
+
+var questionEl = document.getElementById("question");
+var answersEl = document.getElementById("answers");
+var correctnessEl = document.getElementById("correctness");
+var timerEL = document.getElementById("timer");
+var scoreEl = document.getElementById("score");
+var initialsEl = document.getElementById("initials");
+var highscoresEL = document.getElementById("highscores");
+var highscoreArray = [];
+
 
 
