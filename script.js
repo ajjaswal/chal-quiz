827c1933fbf958da5c8a1f2d9dc77d594ae16ef4
AJ Jaswal <jaswalakshay@yahoo.com>
2022-04-23 16:59:59 -0400

    start page elements

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+// page elements
+var startButton = document.querySelector("#start-button");
+var highscoreButton = document.getElementById("highscore-button");
+var backButton = document.getElementById("back-button");
+var submitButton = document.getElementById("submit-button");
+var clearButton = document.getElementById("clear-button");
+
 
 
